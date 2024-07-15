f9c869ce78be8abd2e989f495d26ece5980f1bf6
VECTOR3Studio <47158115+VECTOR3Studio@users.noreply.github.com>
2024-07-15 18:42:52 +0200

    commit

--- php/config.php
+++ php/config.php
@@ -1,1 +1,7 @@
-DB connnection
+<?php 
+    $conn = mysqli_connect("sql13.hostcreators.sk:3318", "u5658_Simon", "Rudinka40-45", "d5658_messaging_app");
+    if($conn){
+    } else{
+        echo "Error" .mysqli_connect_error();
+    }
+?>
